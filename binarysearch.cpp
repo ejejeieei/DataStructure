b4f0427246c0741903fda24baba6f32cf9e2fa72
ejejeieei <1613387231@qq.com>
2024-08-25 22:58:32 +0800

    2024-8-25

---
+++ binarysearch.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size()-1;
+        int middle = 0;
+        while (left <= right)
+        {
+            middle = (left+right)/2;
+            if(nums[middle] == target) return middle;
+            else if(nums[middle]<target){left = middle + 1;}
+            else right = middle - 1;
+        }
+        return -1;
+    }
+};
